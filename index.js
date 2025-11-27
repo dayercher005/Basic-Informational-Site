ef5e9325495ffb30d76516d5c82ca1a13ffa8a72
Dayer <dayerxr.cher@gmail.com>
2025-11-27 10:31:47 +0800

    Fixed server

--- index.js
+++ index.js
@@ -7,37 +7,54 @@ const server = http.createServer((request, response) => {
     const currentURL = request.url
 
     switch(currentURL) {
-        case "./":
-            fs.readFile('index.html', 'utf-8', (error, data) => {
+        case "/":
+            fs.readFile('./index.html', 'utf-8', (error, data) => {
                 if(error){
-
+                    response.writeHead(404);
+                    response.end("Error: File Not Found");
+                } else {
+                    response.end(data)
                 }
             })
+            break;
 
-        case "./about":
-            fs.readFile('about.html', 'utf-8', (error, data) => {
+        case "/about":
+            fs.readFile('./about.html', 'utf-8', (error, data) => {
                 if(error){
-
+                    response.writeHead(404);
+                    response.end("Error: File Not Found");
+                } else {
+                    response.end(data)
                 }
             })
+            break;
 
-        case "./contact-me":
-            fs.readFile('contact-me.html', 'utf-8', (error, data) => {
+        case "/contact-me":
+            fs.readFile('./contact-me.html', 'utf-8', (error, data) => {
                 if (error){
-
+                    response.writeHead(404);
+                    response.end("Error: File Not Found");
+                } else {
+                    response.end(data)
                 }
             })
+            break;
 
         default:
             fs.readFile('404.html', 'utf-8', (error, data) => {
                 if (error){
-                    
+                    response.writeHead(404);
+                    response.end("Error: File Not Found");
+                } else {
+                    response.end(data)
                 }
             })
+            break;
     }
 
     response.statusCode = 200;
-    response.setHeader("Content-Type", "text/plain");
+    response.setHeader("Content-Type", "text/html");
+}).listen(8080, () => {
+    console.log("Server is running on http://localhost:8080")
 });
 
-server.listen(8080)
